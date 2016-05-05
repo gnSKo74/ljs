ce82f4a871d2dc947bcde2b7f95eb60e4fc989eb
gnSKo74 <arbenita_koln@hotmail.com>
2016-05-05 17:12:20 +0200

    c2

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 const gulp = require('gulp');
+const babel = require('gulp-babel');
 // Gulp dependencies go here
 gulp.task('default', function() {
-		//Gulp tasks go here
+		//Node code
+		gulp.src("es6/**/*.js")
+			.pipe(babel())
+			.pipe(gulp.dest("dist"));
+		//browser source
+		gulp.src("public/es6/**/*.js")
+			.pipe(babel())
+			.pipe(gulp.dest("public/dist"));
 	});
